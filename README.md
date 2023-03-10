24f16e9a02b0fb738547ea9fdbfa82f4e2127965
uliseshs1202 <124804941+uliseshs1202@users.noreply.github.com>
2023-03-09 21:55:19 -0600

    Add files via upload
    02/03/2023

---
+++ README.md
@@ -0,0 +1,2 @@
+# WEB1_2557
+Clases de programacion Web ICO
---
+++ nombre.java
@@ -0,0 +1,9 @@
+public class nombre{
+    public static void main(String args[]){
+        int a=10;
+        int b = 10;
+        int c= a+b;
+        //holi
+         System.out.println(c);
+    }
+}
